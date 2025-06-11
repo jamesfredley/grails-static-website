0c124c660de16d88c28e8f16e29b68ba997d16ca
Paul King <paulk@asert.com.au>
2025-06-11 15:06:23 +1000

    first draft at updated download page

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+<!--
+SPDX-License-Identifier: Apache-2.0
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+-->
+
 # Grails Website
 
 [![Build Status](https://github.com/apache/grails-static-website/workflows/Publish/badge.svg)](https://github.com/apache/grails-static-website/actions)
@@ -22,6 +38,7 @@ You can get more info by running:
  buildGuides - Build guides website - generates guides pages, copies assets and generates a sitemap
  copyAssets - Copies css, js, fonts and images from the assets folder to the dist folder
  genDocs - Generates documentation HTML page - build/temp/documentation.html
+ genDownloads - Generates download HTML page - build/temp/download.html
  genFaq - Generates FAQ HTML - build/temp/faq.html
  genGuides - Generates guides home, tags and categories HTML pages - build/temp/index.html
  genProfiles - Generates profiles HTML page - build/temp/profiles.html
---
+++ buildSrc/src/main/groovy/org/grails/documentation/DownloadPage.groovy
@@ -0,0 +1,151 @@
+/*
+ *  Licensed to the Apache Software Foundation (ASF) under one
+ *  or more contributor license agreements.  See the NOTICE file
+ *  distributed with this work for additional information
+ *  regarding copyright ownership.  The ASF licenses this file
+ *  to you under the Apache License, Version 2.0 (the
+ *  "License"); you may not use this file except in compliance
+ *  with the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ */
+package org.grails.documentation
+
+import groovy.transform.CompileDynamic
+import groovy.transform.CompileStatic
+import groovy.xml.MarkupBuilder
+
+@CompileStatic
+class DownloadPage {
+
+    static String binaryUrl(String version, String artifact, String ext = '') {
+        "https://dist.apache.org/repos/dist/release/grails/core/${version}/distribution/apache-${artifact}-${version}-incubating-bin.zip${ext}"
+    }
+
+    static String sourceUrl(String version, String ext = '') {
+        "https://dist.apache.org/repos/dist/release/grails/core/${version}/sources/apache-grails-${version}-incubating-src.zip${ext}"
+    }
+
+    @CompileDynamic
+    static String renderDownload(String version) {
+        StringWriter writer = new StringWriter()
+        MarkupBuilder html = new MarkupBuilder(writer)
+        html.div(class: "guidegroup") {
+            if (version) {
+                boolean isSnapshot = version.endsWith('-SNAPSHOT') || version.contains('snapshot')
+                div(class: "guidegroupheader") {
+                    img(src: "[%url]/images/download.svg", alt: "Download Grails (${version})")
+                    h2 "${isSnapshot ? 'Snapshot' : (version.contains('-M') ? 'Milestone' : (version.contains('-RC') ? 'Release Candidate': 'Latest Stable'))} Version (${version}) Downloads"
+                }
+                ul {
+                    if (version.startsWith('7')) {
+                        li {
+                            a(href: sourceUrl(version), 'Source')
+                            a(href: sourceUrl(version, '.sha512'), 'SHA512')
+                            a(href: sourceUrl(version, '.asc'), 'ASC')
+                        }
+                        li {
+                            a(href: binaryUrl(version, 'grails'), 'Binary')
+                            a(href: binaryUrl(version, 'grails', '.sha512'), 'SHA512')
+                            a(href: binaryUrl(version, 'grails', '.asc'), 'ASC')
+                        }
+                        li {
+                            a(href: binaryUrl(version, 'grails-wrapper'), 'Binary Wrapper')
+                            a(href: binaryUrl(version, 'grails-wrapper', '.sha512'), 'SHA512')
+                            a(href: binaryUrl(version, 'grails-wrapper', '.asc'), 'ASC')
+                        }
+                    } else {
+                        li {
+                            a(href: "https://github.com/apache/grails-forge/releases/download/v${version}/apache-grails-${version}-incubating-bin.zip", 'Binary')
+                        }
+                    }
+                    if (!isSnapshot) {
+                        li {
+                            a(href: "https://github.com/apache/grails-core/releases/tag/v${version}", 'Release Notes')
+                        }
+                    }
+
+                }
+            }
+        }
+        writer.toString()
+    }
+
+
+    @CompileDynamic
+    static String mainContent(File releases) {
+        StringWriter writer = new StringWriter()
+        MarkupBuilder html = new MarkupBuilder(writer)
+
+        html.div(class: 'headerbar chalicesbg') {
+            html.div(class: 'content') {
+                h1 'Downloads'
+            }
+        }
+        SoftwareVersion preRelease = SiteMap.latestPreReleaseVersion(releases)
+        SoftwareVersion latest = SiteMap.latestVersion(releases)
+        html.div(class: 'content') {
+            html.div(class: "twocolumns") {
+                html.div(class: "odd column"){
+                    h3(class: 'columnheader', style: 'margin-bottom: 10px;', 'Source and Binary Releases')
+                    p 'NOTE: Versions prior to 7.0.0-M4 are not ASF releases. Links to those releases are provided here as a convenience.'
+                    mkp.yieldUnescaped "We provide OpenPGP signatures ('.asc') files and checksums ('.sha256') for every release artifact. We recommend that you "
+                    a(href: 'https://www.apache.org/info/verification.html', 'verify')
+                    mkp.yieldUnescaped " the integrity of downloaded files by generating your own checksums and match them against ours, and checking signatures using the "
+                    a(href: 'https://downloads.apache.org/grails/KEYS', 'KEYS')
+                    mkp.yieldUnescaped " file which contains the Grails OpenPGP release keys."
+                    if (preRelease > latest) {
+                        mkp.yieldUnescaped(renderDownload(preRelease.versionText))
+                    }
+
+                    mkp.yieldUnescaped(renderDownload('snapshot'))
+
+                    mkp.yieldUnescaped(renderDownload(latest.versionText))
+
+                    h3(class:'columnheader', 'Older Versions')
+                    p 'You can download previous versions as far back as Grails 1.2.0.'
+                    div(class:'versionselector') {
+                        select(class:'form-control', onchange:"window.location.href=this.value.startsWith('6') ? 'https://github.com/apache/grails-forge/releases/download/v'+this.value+'/grails-cli-'+this.value+'.zip': 'https://github.com/apache/grails-core/releases/download/v'+this.value+'/grails-'+this.value+'.zip'") {
+                            option(label:'Select a version', disabled:'disabled', selected:'selected')
+                            SiteMap.stableVersions(releases)*.versionText.each {
+                                option(value: it, it)
+                            }
+                        }
+                    }
+                }
+                html.div(class: "column") {
+
+                    h3(class: 'columnheader', style: 'margin-bottom: 10px;', 'Other ways to get Grails')
+
+                    mkp.yieldUnescaped'Select a profile and set of features tailored to your needs with our application initializer, Grails Application Forge'
+                    li {
+                        a(href: 'https://start.grails.org', 'Grails Application Forge')
+                    }
+
+                    h3(class: 'columnheader', style: 'margin-bottom: 10px;', 'Installing with SDKMAN!')
+                    p 'This tool makes installing the Grails framework on any Unix based platform (Mac OSX, Linux, Cygwin, Solaris, or FreeBSD) easy.'
+                    p 'Simply open a new terminal and enter:'
+                    div(class:'code', '$ curl -s https://get.sdkman.io | bash')
+                    p 'Follow the on-screen instructions to complete installation.'
+                    p 'Open a new terminal or type the command:'
+                    div(class:'code', '$ source "$HOME/.sdkman/bin/sdkman-init.sh"')
+                    p 'Then install the latest stable Grails version:'
+                    div(class:'code', '$ sdk install grails')
+                    p 'If prompted, make this your default version. After installation is complete it can be tested with:'
+                    div(class:'code', '$ grails --version')
+                    p "That's all there is to it!"
+                }
+            }
+        }
+
+        writer.toString()
+    }
+
+}
